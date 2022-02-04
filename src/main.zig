55e78fe073ef0de080be477528a0a6c14743700b
Anton Christoffersson <anton_christoffersson@hotmail.com>
2022-02-04 15:13:34 +0100

    Added write_color function.

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const vector = std.meta.Vector;
 
 pub fn main() anyerror!void {
     const image_width: i32 = 256;
@@ -6,20 +7,24 @@ pub fn main() anyerror!void {
 
     const stdout = std.io.getStdOut().writer();
     try stdout.print("P3\n{} {}\n255\n", .{ image_width, image_height });
+    const vec: std.meta.Vector(3, f32) = [_]f32{0.0, 0.0, 0.0};
+    std.log.err("hello: {}", .{vec[0]});
 
     var j: i32 = image_height - 1;
     while (j >= 0) : (j -= 1) {
+        std.log.err("\rScanlines remaining: {}", .{j});
         var i: i32 = 0;
         while (i < image_width) : (i += 1) {
-            const r: f32 = @intToFloat(f32, i) / @intToFloat(f32, image_width - 1);
-            const g: f32 = @intToFloat(f32, j) / @intToFloat(f32, image_height - 1);
-            const b: f32 = 0.25;
-
-            const ir = @floatToInt(i32, 255.999 * r);
-            const ig = @floatToInt(i32, 255.999 * g);
-            const ib = @floatToInt(i32, 255.999 * b);
-
-            try stdout.print("{} {} {}\n", .{ ir, ig, ib });
+            const pixel_color: vector(3, f32) = [_]f32 {@intToFloat(f32, i) / @intToFloat(f32, image_width - 1), @intToFloat(f32, j) / @intToFloat(f32, image_height - 1), 0.25};
+            try write_color(stdout, pixel_color);
         }
     }
+    
+    std.log.err("\rDone.", .{});
+}
+
+fn write_color(out: std.fs.File.Writer, vec: vector(3, f32)) !void {
+    try out.print("{} {} {}\n", .{@floatToInt(i32, vec[0] * 255.999), 
+                                  @floatToInt(i32, vec[1] * 255.999), 
+                                  @floatToInt(i32, vec[2] * 255.999)});
 }
